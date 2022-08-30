40d93ea6ae9cfc1d99f1e8d433e8d37a04ef3262
asheeshcric <ashiz2013@gmail.com>
2022-08-29 21:41:37 -0500

    Test installation of htop by running the script

--- macOS/installation_scripts/basic_applications.sh
+++ macOS/installation_scripts/basic_applications.sh
@@ -1,3 +1,6 @@
 # Install Alfred
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
 brew install --cask alfred
+
+# Install htop
+brew install htop
